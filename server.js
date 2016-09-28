866d6b520a5991f7ce7e790472e0b0d459f5d1ed
Deepak Goyal <goyaldeepu468@gmail.com>
2016-09-29 01:45:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,35 +29,35 @@ function createTemplate(data){
     var date=data.date;
     
     var htmltemplate=`
-        <html>
-            <head>
-                <title>
-                    ${title}
-                </title>
-                <meta name="viewport" content="width-device-width,initial-scale-1"/>
-                 <link href="/ui/style.css" rel="stylesheet" />
-            </head>
-            <body>
-                <div class="container">
-                    <div>
-                        <a href="/">Home</a>
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width-device-width,initial-scale-1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
                         
-                    </div>
-                    <h3>
-                        ${heading};
-                    </h3>
-                    <div>
-                        ${date};
-                    </div>
-                    <div>
-                        ${content};
+                </div>
+                <h3>
+                    ${heading};
+                </h3>
+                <div>
+                    ${date};
+                </div>
+                <div>
+                    ${content};
                         
-                    </div>
+                </div>
                 </div>
                 
-            </body>
-        </html>
-        return htmlTemplate;
+        </body>
+    </html>
+    return htmlTemplate;
 
 `;
 }
