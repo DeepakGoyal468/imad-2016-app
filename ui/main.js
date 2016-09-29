6334ff98c54f285524e5d7d3407f6fb43fa22585
Deepak Goyal <goyaldeepu468@gmail.com>
2016-09-29 14:31:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,10 @@
-console.log('Loaded!');
-
-var element= document.getElementById('main-text');
-
-element.innerHTML='New Value';
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+//counter code
+var button= document.getElementById("counter");
+var counter=0;
+button.onclick=function(){
+    //make a request to the counter endpoint
+    counter = counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
 }
