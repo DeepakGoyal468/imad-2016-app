ba1d6a0aa4e68af6ce540dc38461c7a92355c7a8
Deepak Goyal <goyaldeepu468@gmail.com>
2016-11-26 19:55:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -85,3 +85,12 @@ submit.onclick=function(){
   var ul=document.getElementById('namelist');
   ul.innerHTML=list;
 };
+$(".proBtn").on('click', function()
+{
+  $('.projectDisplay').fadeToggle("slow", "linear");
+  $('.about').fadeToggle("fast", "linear");
+});
+$(".skillsBtn").on('click', function()
+{
+  $(".techDisplay").fadeToggle("slow", "linear");
+});
